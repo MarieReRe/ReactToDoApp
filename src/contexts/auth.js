a96136d8c7bb01306f21764e2f5eda799e6f41e0
Marie Antons <chefantons@gmail.com>
2020-06-25 22:07:29 -0500

    rendering

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -4,15 +4,22 @@ export const AuthContext = React.createContext();
 
 export default function useAuth() {
     return useContext(AuthContext);
-  }
+}
 
 
-  export class AuthProvider extends React.Component {
+export class AuthProvider extends React.Component {
     constructor(props) {
-      super(props);
-  
-      this.state = {
-        user: null,
-      };
+        super(props);
+
+        this.state = {
+            user: null,
+        };
     }
-  
+    render() {
+        return (
+            <AuthContext.Provider value={this.state}>
+                {this.props.children}
+            </AuthContext.Provider>
+        );
+    }
+}

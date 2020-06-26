8d3d58124a3041a533bdf4a33c6645bdfccd86a4
Marie Antons <chefantons@gmail.com>
2020-06-25 21:12:19 -0500

    handleSubmit built

--- src/auth/login.js
+++ src/auth/login.js
@@ -3,5 +3,12 @@ import React from 'react';
 
 
 export default function Login(){
+    //Handle Submission
+    handleSubmit = (event => {
+        //prevent the default
+        event.preventDefault();
+
+        const {username, password} = event.target.elements;
+    })
 
 }

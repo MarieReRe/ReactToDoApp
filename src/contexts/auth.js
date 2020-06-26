3665912698fdca05de69617ffce8c438bac0986e
Marie Antons <chefantons@gmail.com>
2020-06-25 21:27:22 -0500

    authProvider setup

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -5,4 +5,14 @@ export const AuthContext = React.createContext();
 export default function useAuth() {
     return useContext(AuthContext);
   }
+
+
+  export class AuthProvider extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        user: null,
+      };
+    }
   
